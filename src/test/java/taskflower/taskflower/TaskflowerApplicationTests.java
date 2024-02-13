ac69309213a335a0229451a8dc634fbb78180a2a
codeartitect <ysw991106@gmail.com>
2024-02-13 22:03:09 +0900

    project init

---
+++ src/test/java/taskflower/taskflower/TaskflowerApplicationTests.java
@@ -0,0 +1,13 @@
+package taskflower.taskflower;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaskflowerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
