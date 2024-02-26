4ef9e81239b0c09a6b54e962ddc63dc818bdebcd
codeartitect <ysw991106@gmail.com>
2024-02-26 15:17:34 +0900

    test: TaskMapper 적용후 테스트 코드 재생성

--- src/test/java/taskflower/taskflower/task/TaskServiceTest.java
+++ src/test/java/taskflower/taskflower/task/TaskServiceTest.java
@@ -1,13 +1,11 @@
 package taskflower.taskflower.task;
 
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.security.test.context.support.WithUserDetails;
-
-import java.time.ZonedDateTime;
 
 @SpringBootTest
 class TaskServiceTest {
@@ -16,32 +14,50 @@ class TaskServiceTest {
     private TaskService taskService;
 
     @Test
-    @WithUserDetails(value = "test@test.test")
-//    @WithMockUser(username = "test@test.test", password = "1234")
+    @WithUserDetails(value = "test@test.test")      // Test userDetail(security test)
+    @DisplayName("Task 생성 및 조화")
     void createTask() throws TaskNotFoundExeption {
         SaveTaskRequset saveTaskRequset = new SaveTaskRequset();
         saveTaskRequset.setTitle("test title");
         saveTaskRequset.setDescription("test description.....");
         saveTaskRequset.setStatus(Status.TODO);
-        saveTaskRequset.setStartTime("2024-02-16T10:15:30 Asia/Seoul");
-        saveTaskRequset.setEndTime("2024-03-16T10:15:30 Asia/Seoul");
+        saveTaskRequset.setStartTime(new int[]{2024, 2, 16, 10, 15});
+        saveTaskRequset.setEndTime(new int[]{2024, 3, 16, 10, 15});
         saveTaskRequset.setTag("test tag");
 
         Task savedTask = taskService.save(saveTaskRequset);
 
         System.out.println(savedTask.toString());
 
-//        Assertions.assertEquals(savedTask, taskService.getTaskById(savedTask.getId()));
-
+        Assertions.assertEquals(savedTask, taskService.getTaskById(savedTask.getId()));
     }
 
 
     @Test
-    void timeTest() {
-        String date = "2024-02-16T10:15:30 Asia/Seoul";
-        ZonedDateTime time = ZonedDateTime.parse(date);
+    @WithUserDetails(value = "test@test.test")
+    void updateTask() throws TaskNotFoundExeption {
+        SaveTaskRequset saveTaskRequset = new SaveTaskRequset();
+        saveTaskRequset.setTitle("test title");
+        saveTaskRequset.setDescription("test description.....");
+        saveTaskRequset.setStatus(Status.TODO);
+        saveTaskRequset.setStartTime(new int[]{2024, 2, 16, 10, 15});
+        saveTaskRequset.setEndTime(new int[]{2024, 3, 16, 10, 15});
+        saveTaskRequset.setTag("test tag");
 
-        System.out.println(time.toString());
+        Task savedTask = taskService.save(saveTaskRequset);
+
+        SaveTaskRequset updateTaskRequset = new SaveTaskRequset();
+        updateTaskRequset.setTitle("abcdefg");
+        updateTaskRequset.setDescription("............description");
+        updateTaskRequset.setStatus(Status.PROGRESS);
+        updateTaskRequset.setStartTime(new int[]{2024, 2, 19, 10, 15});
+        updateTaskRequset.setEndTime(new int[]{2024, 3, 11, 10, 15});
+        updateTaskRequset.setTag("test tag");
+
+        Task updatedTask = taskService.updateTask(savedTask.getId(), updateTaskRequset);
+
+        Assertions.assertEquals(updatedTask, taskService.getTaskById(savedTask.getId()));
+
     }
 
 }
