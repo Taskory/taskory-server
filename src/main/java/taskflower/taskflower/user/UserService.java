82e2079b3217094add17c2bd3e716a2bf6458b06
codeartitect <ysw991106@gmail.com>
2024-02-26 16:17:26 +0900

    feat: 권한을 이용하여 사용자 정보 조회 기능

--- src/main/java/taskflower/taskflower/user/UserService.java
+++ src/main/java/taskflower/taskflower/user/UserService.java
@@ -1,7 +1,10 @@
 package taskflower.taskflower.user;
 
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
+import taskflower.taskflower.security.UserDetailsImpl;
 import taskflower.taskflower.user.exception.UserNotFoundException;
 
 @Service
@@ -40,5 +43,11 @@ public class UserService {
         // 삭제 성공 시, 결과 반환x, 실패할 경우 예외 처리
         User user = getUserById(id);
         userRepository.delete(user);
+    }
+
+    public User findUserByAuth() {
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        UserDetailsImpl principal = (UserDetailsImpl) authentication.getPrincipal();
+        return userRepository.findUserByEmail(principal.getEmail());
     }
 }
