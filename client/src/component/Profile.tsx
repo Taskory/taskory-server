86c38272d4b0d4ad77a54c4c17ced6b263db9591
codeartitect <ysw991106@gmail.com>
2024-02-28 11:13:19 +0900

    test: cookie가 잘 넘어오는지 확인

--- client/src/component/Profile.tsx
+++ client/src/component/Profile.tsx
@@ -1,4 +1,13 @@
+import {useCookies} from "react-cookie";
+import {useEffect} from "react";
+
 export const Profile: React.FC = () => {
+  const [cookies] = useCookies(['token']);
+
+  useEffect(() => {
+    console.log(cookies.token);
+  }, [cookies]);
+
   return (
     <>
     </>
