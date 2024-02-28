a11d53daf3d1e7bda73f67ac79a5608abc3d2a39
codeartitect <ysw991106@gmail.com>
2024-02-28 17:23:54 +0900

    feat: 토큰으로 프로필 조회 기능

--- client/src/component/Profile.tsx
+++ client/src/component/Profile.tsx
@@ -2,11 +2,51 @@ import {useCookies} from "react-cookie";
 import {useEffect} from "react";
 
 export const Profile: React.FC = () => {
-  const [cookies] = useCookies(['token']);
+  const [cookies, setCookie] = useCookies(['token']);
+
 
   useEffect(() => {
-    console.log(cookies.token);
-  }, [cookies]);
+    const fetchLogin = async () => {
+      const requestOptions = {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json"
+        },
+        body: JSON.stringify({
+          email: "seongwon@gmail.com",
+          password: "1234"
+        }),
+      }
+      try {
+        await fetch("http://localhost:8080/api/v1/auth/login", requestOptions).then(res => {
+          if (res.ok) {
+            res.json().then(result => {
+              setCookie('token', result.token);
+              // navigate("/");
+            });
+          } else {
+            alert("이메일 및 비밀번호가 잘못되었습니다.")
+          }
+        })
+      } catch (e) {
+        alert("로그인 오류");
+      }
+    }
+
+    fetchLogin();
+    const requsetOptions = {
+      headers: {
+        'Content-Type': 'application/json',
+        'Authorization': 'Bearer ' + cookies.token
+      },
+      method: 'GET'
+    }
+    // console.log(cookies.token);
+    fetch("http://localhost:8080/api/v1/user/profile", requsetOptions)
+      .then(res => {
+        // console.log(res);
+      })
+  }, []);
 
   return (
     <>
