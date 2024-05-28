0df92f33543a4dd38d4f40e040a5fbd9e759b7f3
codeartitect <ysw991106@gmail.com>
2024-05-28 15:21:27 +0900

    feat: test for controller

--- src/main/java/codeartitect/taskflower/user/UserController.java
+++ src/main/java/codeartitect/taskflower/user/UserController.java
@@ -1,4 +1,12 @@
 package codeartitect.taskflower.user;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("${app.url-base}/user")
 public class UserController {
+
 }
