54d64a573c6d21817abc53c369764fa68b269129
codeartist99 <ysw991106@gmail.com>
2024-06-20 19:25:39 +0900

    feat: add footer to common layout

--- client/src/layout/CommonLayout.tsx
+++ client/src/layout/CommonLayout.tsx
@@ -2,6 +2,7 @@ import React, { ReactNode } from 'react';
 import { LeftBar } from '../component/LeftBar';
 import { Header } from '../component/Header';
 import { RightBar } from '../component/RightBar';
+import {Footer} from "../component/Footer";
 
 interface CommonLayoutProps {
     children: ReactNode;
@@ -9,17 +10,20 @@ interface CommonLayoutProps {
 
 export const CommonLayout: React.FC<CommonLayoutProps> = ({ children }) => {
     return (
-        <div className="flex min-h-screen">
-            <LeftBar />
-            <div className="flex flex-col flex-grow">
-                <Header />
-                <div className="flex flex-grow">
-                    <main className="flex-grow p-4 bg-gray-100">
-                        {children}
-                    </main>
-                    <RightBar />
+        <>
+            <div className="flex min-h-screen">
+                <LeftBar/>
+                <div className="flex flex-col flex-grow">
+                    <Header/>
+                    <div className="flex flex-grow">
+                        <main className="flex-grow p-4 bg-gray-100">
+                            {children}
+                        </main>
+                        <RightBar/>
+                    </div>
                 </div>
             </div>
-        </div>
+            <Footer/>
+        </>
     );
 };
