3e6a83d825c473ab36751092bb324227d53df067
codeartist99 <ysw991106@gmail.com>
2024-06-20 18:30:44 +0900

    feat: implement auth redirect

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,28 +1,11 @@
-import React from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import {Home} from "./page/home/Home";
-import {Login} from "./page/auth/Login";
-import {Signup} from "./page/auth/Signup";
-import {Dashboard} from "./page/dashboard/Dashboard";
-import {Calendar} from "./page/calendar/Calendar";
-import {Routine} from "./page/routine/Routine";
-import {Task} from "./page/task/Task";
-
+import React, {useEffect} from 'react';
+import {AppRouter} from "./AppRouter";
 
 const App = () => {
     return (
-        <Router>
-            <Routes>
-                <Route path="/" element={<Home/>}/>
-                <Route path="/login" element={<Login/>}/>
-                <Route path="/signup" element={<Signup/>}/>
-                <Route path="/dashboard" element={<Dashboard/>}/>
-                <Route path="/calendar" element={<Calendar/>}/>
-                <Route path="/routine" element={<Routine/>}/>
-                <Route path="/task" element={<Task/>}/>
-            </Routes>
-        </Router>
-    );
+        <>
+            <AppRouter />
+        </>
+    )
 }
-
 export default App;
---
+++ client/src/AppRouter.tsx
@@ -0,0 +1,27 @@
+import {BrowserRouter as Router, Route, Routes} from "react-router-dom";
+import {Home} from "./page/home/Home";
+import {Login} from "./page/auth/Login";
+import {Signup} from "./page/auth/Signup";
+import {Dashboard} from "./page/dashboard/Dashboard";
+import {Calendar} from "./page/calendar/Calendar";
+import {Routine} from "./page/routine/Routine";
+import {Task} from "./page/task/Task";
+import React from "react";
+import {AuthRedirectHandler} from "./handler/AuthRedirectHandler";
+
+export const AppRouter = (): React.JSX.Element => {
+    return (
+        <Router>
+            <AuthRedirectHandler />
+            <Routes>
+                <Route path="/" element={<Home/>}/>
+                <Route path="/login" element={<Login/>}/>
+                <Route path="/signup" element={<Signup/>}/>
+                <Route path="/dashboard" element={<Dashboard/>}/>
+                <Route path="/calendar" element={<Calendar/>}/>
+                <Route path="/routine" element={<Routine/>}/>
+                <Route path="/task" element={<Task/>}/>
+            </Routes>
+        </Router>
+    );
+}
