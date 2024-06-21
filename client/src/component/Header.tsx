e63a8c600bb63833cc0c4126433d891328fa8540
codeartist99 <ysw991106@gmail.com>
2024-06-21 17:52:18 +0900

    feat: implement detailed header component

--- client/src/component/Header.tsx
+++ client/src/component/Header.tsx
@@ -11,16 +11,21 @@ export const Header: React.FC<HeaderProps> = ({ isOpened }) => {
                 isOpened ? 'left-24' : 'left-72'
             } right-0 h-16 transition-all duration-300`}
         >
-            <div className="flex items-center">
-                <input
-                    type="text"
-                    placeholder="Search"
-                    className="border rounded px-4 py-2 mr-4"
-                />
+            <div className="flex items-center w-full">
+                <div className="flex border rounded w-1/4 px-2 items-center">
+                    <input
+                        type="text"
+                        placeholder="Search"
+                        className="px-2 py-1 border-none outline-none flex-grow"
+                    />
+                    <button className="btn btn-circle btn-sm border-none bg-base-100 h-10 w-10 flex items-center justify-center">
+                        <img src="/asset/img/header/search.svg" alt="search" className="h-6 w-6"/>
+                    </button>
+                </div>
             </div>
             <div className="flex items-center">
-                <img src="/asset/img/Logo.png" alt="Logo" className="h-8 w-8 mr-2" />
-                <img src="/asset/img/Logo.png" alt="Logo" className="h-8 w-8 mr-2" />
+                <img src="/asset/img/header/notification.svg" alt="notification" className="h-8 w-8 mr-2"/>
+                <img src="/asset/img/Logo.png" alt="Logo" className="h-8 w-8 mr-2"/>
             </div>
         </header>
     );
