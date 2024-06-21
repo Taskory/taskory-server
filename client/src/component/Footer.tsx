321fb1c03ec99b9d9258cd5dc1bd215064e2d28c
codeartist99 <ysw991106@gmail.com>
2024-06-21 17:26:17 +0900

    feat: implement detailed footer

--- client/src/component/Footer.tsx
+++ client/src/component/Footer.tsx
@@ -1,46 +1,44 @@
 
 export const Footer = () => {
     return (
-        <footer className="bg-white border-t border-gray-200 px-4 py-2 text-center w-full h-12">
-            <div className="container mx-auto flex justify-between items-center">
-                <div className="flex space-x-4">
-                    <a href="#" className="text-gray-600 hover:text-gray-900">About Us</a>
-                    <a href="#" className="text-gray-600 hover:text-gray-900">Features</a>
-                    <a href="#" className="text-gray-600 hover:text-gray-900">Pricing</a>
-                    <a href="#" className="text-gray-600 hover:text-gray-900">Blog</a>
-                    <a href="#" className="text-gray-600 hover:text-gray-900">Contact Us</a>
-                </div>
-                <div className="flex space-x-4">
-                    <a href="#" className="text-gray-600 hover:text-gray-900">
+        <footer className="footer p-10 bg-base-300">
+            <aside>
+                <img
+                    src="/asset/img/Logo.png"
+                    alt="logo"
+                    className={`h-24 w-24`}
+                />
+                <p>© 2024 codeartist99. All rights reserved.</p>
+            </aside>
+            <nav>
+                <h6 className="footer-title text-black">Social</h6>
+                <div className="grid grid-flow-col gap-4">
+                    <a href="https://github.com/codeartist99" className="text-gray-600 hover:text-gray-900">
                         <img
-                            src="/asset/img/Logo.png"
-                            alt="Logo"
-                            className={`h-8 w-8 mr-2`}
+                            src="/asset/img/social/github.svg"
+                            alt="github"
+                            className={`h-6 w-6`}
                         />
                     </a>
-                    <a href="#" className="text-gray-600 hover:text-gray-900">
+                    <a href="https://www.linkedin.com/in/seongwon-yang-01928a228/"
+                       className="text-gray-600 hover:text-gray-900">
                         <img
-                            src="/asset/img/Logo.png"
-                            alt="Logo"
-                            className={`h-8 w-8 mr-2`}
+                            src="/asset/img/social/linkedin.svg"
+                            alt="linkedin"
+                            className={`h-6 w-6`}
                         />
                     </a>
-                    <a href="#" className="text-gray-600 hover:text-gray-900">
+                    <a href="https://www.instagram.com/99_seongwon"
+                       className="text-gray-600 hover:text-gray-900">
                         <img
-                            src="/asset/img/Logo.png"
-                            alt="Logo"
-                            className={`h-8 w-8 mr-2`}
-                        />
-                    </a>
-                    <a href="#" className="text-gray-600 hover:text-gray-900">
-                        <img
-                            src="/asset/img/Logo.png"
-                            alt="Logo"
-                            className={`h-8 w-8 mr-2`}
+                            src="/asset/img/social/instagram.svg"
+                            alt="instagram"
+                            className={`h-6 w-6`}
                         />
                     </a>
                 </div>
-            </div>
+            </nav>
         </footer>
-    );
+    )
+        ;
 };
