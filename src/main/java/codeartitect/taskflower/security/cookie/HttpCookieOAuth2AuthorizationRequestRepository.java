13d28c04913e497290bc75510d06228021469e89
codeartist99 <ysw991106@gmail.com>
2024-06-24 16:31:15 +0900

    style: implement log about oauth2 login logic

--- src/main/java/codeartitect/taskflower/security/cookie/HttpCookieOAuth2AuthorizationRequestRepository.java
+++ src/main/java/codeartitect/taskflower/security/cookie/HttpCookieOAuth2AuthorizationRequestRepository.java
@@ -2,12 +2,14 @@ package codeartitect.taskflower.security.cookie;
 
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.oauth2.client.web.AuthorizationRequestRepository;
 import org.springframework.security.oauth2.core.endpoint.OAuth2AuthorizationRequest;
 import org.springframework.stereotype.Component;
 import org.springframework.util.StringUtils;
 
 @Component
+@Slf4j
 public class HttpCookieOAuth2AuthorizationRequestRepository implements AuthorizationRequestRepository<OAuth2AuthorizationRequest> {
     public static final String OAUTH2_AUTHORIZATION_REQUEST_COOKIE_NAME = "oauth2_auth_request";
     public static final String REDIRECT_URI_PARAM_COOKIE_NAME = "redirect_uri";
@@ -15,6 +17,7 @@ public class HttpCookieOAuth2AuthorizationRequestRepository implements Authoriza
 
     @Override
     public OAuth2AuthorizationRequest loadAuthorizationRequest(HttpServletRequest request) {
+        log.info("[LOG - HttpCookieOAuth2AuthorizationRequestRepository.loadAuthorizationRequest]");
         return CookieUtil.getCookie(request, OAUTH2_AUTHORIZATION_REQUEST_COOKIE_NAME)
                 .map(cookie -> CookieUtil.deserialize(cookie.getValue(), OAuth2AuthorizationRequest.class))
                 .orElse(null);
@@ -22,6 +25,7 @@ public class HttpCookieOAuth2AuthorizationRequestRepository implements Authoriza
 
     @Override
     public void saveAuthorizationRequest(OAuth2AuthorizationRequest authorizationRequest, HttpServletRequest request, HttpServletResponse response) {
+        log.info("[LOG - HttpCookieOAuth2AuthorizationRequestRepository.saveAuthorizationRequest]");
         if (authorizationRequest == null) {
             CookieUtil.deleteCookie(request, response, OAUTH2_AUTHORIZATION_REQUEST_COOKIE_NAME);
             CookieUtil.deleteCookie(request, response, REDIRECT_URI_PARAM_COOKIE_NAME);
@@ -37,6 +41,7 @@ public class HttpCookieOAuth2AuthorizationRequestRepository implements Authoriza
 
     @Override
     public OAuth2AuthorizationRequest removeAuthorizationRequest(HttpServletRequest request, HttpServletResponse response) {
+        log.info("[LOG - HttpCookieOAuth2AuthorizationRequestRepository.removeAuthorizationRequest]");
         return this.loadAuthorizationRequest(request);
     }
 }
