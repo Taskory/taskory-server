36e0c69e300a4df6fabfdf71268f04dad8456fe4
codeartist99 <ysw991106@gmail.com>
2024-06-25 19:50:32 +0900

    feat: implement profile or profile update

--- client/src/component/Header.tsx
+++ client/src/component/Header.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
-import {existAuthCookie, removeAuthCookie} from "../util/CookieUtil";
-import {useNavigate} from "react-router-dom";
+import { existAuthCookie, removeAuthCookie } from "../util/CookieUtil";
+import { useNavigate } from "react-router-dom";
 
 interface HeaderProps {
     isOpened: boolean;
@@ -13,7 +13,11 @@ export const Header: React.FC<HeaderProps> = ({ isOpened }) => {
             removeAuthCookie();
             navigate("/");
         }
-    }
+    };
+
+    const navigateToProfile = () => {
+        navigate("/profile");
+    };
 
     return (
         <header
@@ -21,26 +25,36 @@ export const Header: React.FC<HeaderProps> = ({ isOpened }) => {
                 isOpened ? 'left-24' : 'left-72'
             } right-0 h-16 transition-all duration-300`}
         >
-            <div className="flex items-center w-full">
-                <div className="flex border rounded w-1/4 px-2 items-center">
+            <div className="flex items-center w-full space-x-4">
+                <div className="flex border rounded-lg px-2 items-center w-1/4">
                     <input
                         type="text"
                         placeholder="Search"
                         className="px-2 py-1 border-none outline-none flex-grow"
                     />
                     <button
-                        className="btn btn-circle btn-sm border-none bg-base-100 h-10 w-10 flex items-center justify-center">
-                        <img src="/asset/img/header/search.svg" alt="search" className="h-6 w-6"/>
+                        className="btn btn-circle btn-sm bg-base-100 border-none"
+                    >
+                        <img src="/asset/img/header/search.svg" alt="search" className="h-6 w-6" />
                     </button>
                 </div>
             </div>
-            <div className="flex items-center">
-                <img src="/asset/img/header/notification.svg" alt="notification" className="h-8 w-8 mr-2"/>
-                <img src="/asset/img/Logo.png" alt="Logo" className="h-8 w-8 mr-2"/>
+            <div className="flex items-center space-x-4 mr-10">
+                <button className="btn btn-circle bg-white">
+                    <img src="/asset/img/header/notification.svg" alt="notification" className="h-8 w-8"/>
+                </button>
+                <button className="btn bg-white">
+                    <img
+                        src="/asset/img/Logo.png"
+                        alt="Logo"
+                        className="h-8 w-8"
+                        onClick={navigateToProfile}
+                    />
+                </button>
+                <button className="bg-blue-500 text-white px-4 py-2 rounded" onClick={handleLogout}>
+                    Logout
+                </button>
             </div>
-            <button className="bg-blue-500 text-white px-4 py-2 rounded" onClick={handleLogout}>
-                Logout
-            </button>
         </header>
     );
 };
--- client/src/page/profile/Profile.tsx
+++ client/src/page/profile/Profile.tsx
@@ -1,15 +1,10 @@
 import React, {useEffect, useState} from "react";
 import {CommonLayout} from "../../layout/CommonLayout";
-import {existAuthCookie, getAuthCookie} from "../../util/CookieUtil";
+import {existAuthCookie} from "../../util/CookieUtil";
 import {useNavigate} from "react-router-dom";
-import {API_URL} from "../../constants";
-
-interface UserInfoInterface {
-    id: number | null;
-    username: string | null;
-    zoneId: string | null;
-}
-
+import {UserInfoInterface} from "../../api/interface/UserInfoInterface";
+import {requestProfile} from "../../api/user/ApiUtil";
+import timezone from "../../constants/timezone.json";
 
 export const Profile: React.FC = () => {
     const [userInfo, setUserInfo] = useState<UserInfoInterface>({id: null, username: null, zoneId: null});
@@ -20,37 +15,39 @@ export const Profile: React.FC = () => {
             navigate("/");
         }
 
-        const requestOptions = {
-            method: "GET",
-            headers: {
-                "Content-Type": "application/json",
-                "Authorization": "Bearer " + getAuthCookie(),
-            },
-        }
+        requestProfile().then((result: UserInfoInterface) => {
+            setUserInfo({
+                id: result.id,
+                username: result.username,
+                zoneId: result.zoneId
+            })
+        })
 
-        const requestProfile = () => {
-            try {
-                fetch(API_URL + "/user/profile", requestOptions)
-                    .then(res => {
-                        if (res.ok) {
-                            res.json()
-                                .then(result => {
-                                    console.log(result);
-                                    setUserInfo({id: result.id, username: result.username, zoneId: result.zoneId});
-                                });
-                        }
-                    });
-            } catch (error) {
-                console.error(error);
-            }
-        };
-        requestProfile();
     }, [navigate]);
+
+    const navigateToProfileUpdate = () => {
+        navigate("/profile/update");
+    };
+
+    const findCityByZoneId = (zoneId: string | null) => {
+        if (zoneId) {
+            const timezones = timezone.timezone;
+            // Iterate over the keys in the timezone object
+            for (const [city, zone] of Object.entries(timezones)) {
+                if (zone === zoneId) {
+                    return city; // Return the key if the value matches
+                }
+            }
+        }
+        return null; // Return null if no matching value is found
+    };
     return (
         <CommonLayout>
             <div className="max-w-4xl mx-auto p-4 sm:p-6 lg:p-8 bg-white shadow-md rounded-lg">
-                <h2 className="text-2xl font-bold mb-6">Profile</h2>
-
+                <div className="px-4 py-2 flex justify-between items-center">
+                    <h2 className="text-2xl font-bold mb-6">Profile</h2>
+                    <button className="btn" onClick={navigateToProfileUpdate}>Update</button>
+                </div>
                 <div className="flex flex-col md:flex-row items-center md:items-start">
                     <img
                         className="w-32 h-32 rounded-full mb-4 md:mb-0"
@@ -58,49 +55,33 @@ export const Profile: React.FC = () => {
                         alt="Profile"
                     />
                     <div className="md:ml-6">
-                        <h3 className="text-xl font-semibold">John Doe</h3>
+                        <h3 className="text-xl font-semibold">{userInfo.username || ""}</h3>
                         <p className="text-gray-600">john.doe@example.com</p>
                     </div>
                 </div>
 
                 <div className="mt-6">
                     <h4 className="text-lg font-semibold mb-4">Personal Information</h4>
-                    <div className="grid grid-cols-1 sm:grid-cols-2 gap-4">
+                    <div className="grid gap-4 py-4">
                         <div>
-                            <label className="block text-sm font-medium text-gray-700">Full Name</label>
-                            <input
-                                type="text"
-                                className="mt-1 p-2 block w-full shadow-sm sm:text-sm border-gray-300 rounded-md"
-                                value="John Doe"
-                                readOnly
-                            />
+                            <label className="block text-sm font-medium text-gray-700">Username</label>
+                            <p className="mt-1 p-2 block w-full shadow-sm sm:text-sm border-gray-300 rounded-md">{userInfo.username || ""}</p>
                         </div>
                         <div>
                             <label className="block text-sm font-medium text-gray-700">Email</label>
-                            <input
-                                type="email"
-                                className="mt-1 p-2 block w-full shadow-sm sm:text-sm border-gray-300 rounded-md"
-                                value="john.doe@example.com"
-                                readOnly
-                            />
+                            <p className="mt-1 p-2 block w-full shadow-sm sm:text-sm border-gray-300 rounded-md">"john.doe@example.com"</p>
                         </div>
                         <div>
                             <label className="block text-sm font-medium text-gray-700">Phone</label>
-                            <input
-                                type="tel"
-                                className="mt-1 p-2 block w-full shadow-sm sm:text-sm border-gray-300 rounded-md"
-                                value="+1234567890"
-                                readOnly
-                            />
+                            <p className="mt-1 p-2 block w-full shadow-sm sm:text-sm border-gray-300 rounded-md">"+1234567890"</p>
                         </div>
                         <div>
                             <label className="block text-sm font-medium text-gray-700">Address</label>
-                            <input
-                                type="text"
-                                className="mt-1 p-2 block w-full shadow-sm sm:text-sm border-gray-300 rounded-md"
-                                value="123 Main St, Anytown, USA"
-                                readOnly
-                            />
+                            <p className="mt-1 p-2 block w-full shadow-sm sm:text-sm border-gray-300 rounded-md">"123 Main St, Anytown, USA"</p>
+                        </div>
+                        <div>
+                            <label className="block text-sm font-medium text-gray-700">Timezone</label>
+                            <p className="mt-1 p-2 block w-full shadow-sm sm:text-sm border-gray-300 rounded-md">{findCityByZoneId(userInfo.zoneId)}</p>
                         </div>
                     </div>
                 </div>
