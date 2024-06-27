a88af852eacb8f1455d36739a32a2b0338337ae5
codeartist99 <ysw991106@gmail.com>
2024-06-27 14:44:09 +0900

    fix: fix calendar swip bug

--- client/src/page/calendar/component/month/MonthCalendar.tsx
+++ client/src/page/calendar/component/month/MonthCalendar.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, useRef } from "react";
+import React, {useEffect, useRef, useCallback, useState} from "react";
 import { MonthHeader } from "./component/MonthHeader";
 import { Day } from "./component/Day";
 import { useCalendar } from "../../context/CalendarContext";
@@ -6,33 +6,39 @@ import { useSpring, animated } from "react-spring";
 
 export const MonthCalendar: React.FC = () => {
     const { firstDayOfWeek, lastDayOfMonth, events, currentDate, setCurrentDate } = useCalendar();
-    const [isSwiping, setIsSwiping] = useState(false);
     const containerRef = useRef<HTMLDivElement>(null);
+    const [ableScroll, setAbleScroll] = useState<boolean>(true);
+    const [{ x }] = useSpring(() => ({ x: 0 }));
 
-    const [{ x }, set] = useSpring(() => ({ x: 0 }));
-
-    const handleWheel = (event: WheelEvent) => {
-        if (event.deltaY < 0) {
-            // Scroll up
-            setCurrentDate(new Date(currentDate.getFullYear(), currentDate.getMonth() - 1, 1));
-        } else if (event.deltaY > 0) {
-            // Scroll down
-            setCurrentDate(new Date(currentDate.getFullYear(), currentDate.getMonth() + 1, 1));
+    const handleWheel = useCallback((event: WheelEvent) => {
+        if (ableScroll) {
+            setAbleScroll(false);
+            event.preventDefault(); // Prevent the default wheel behavior
+            setTimeout(() => {
+                if (event.deltaY < -10) {
+                    // Scroll up
+                    setCurrentDate(new Date(currentDate.getFullYear(), currentDate.getMonth() - 1, 1));
+                } else if (event.deltaY > 10) {
+                    // Scroll down
+                    setCurrentDate(new Date(currentDate.getFullYear(), currentDate.getMonth() + 1, 1));
+                }
+            }, 500);
         }
-    };
+        setAbleScroll(true);
+    }, [currentDate, setCurrentDate]);
 
     useEffect(() => {
         const container = containerRef.current;
         if (container) {
-            container.addEventListener("wheel", handleWheel);
+            container.addEventListener("wheel", handleWheel, { passive: false });
             return () => container.removeEventListener("wheel", handleWheel);
         }
-    }, [currentDate]);
+    }, [handleWheel]);
 
     return (
         <animated.div ref={containerRef} style={{ x }}>
             <MonthHeader />
-            <div className={`grid grid-cols-7 gap-4 border px-4 py-4 ${isSwiping ? "pointer-events-none" : ""}`}>
+            <div className="grid grid-cols-7 gap-4 border px-4 py-4">
                 {Array(firstDayOfWeek).fill(null).map((_, index) => (
                     <div key={index} className="border p-2 h-32"></div>
                 ))}
