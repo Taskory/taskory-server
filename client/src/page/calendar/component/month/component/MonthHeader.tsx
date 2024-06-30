81d859e41b4d96a19ec283b6681553bc85f83b9f
codeartist99 <ysw991106@gmail.com>
2024-06-30 17:20:32 +0900

    feat: color sat, sun in month header

--- client/src/page/calendar/component/month/component/MonthHeader.tsx
+++ client/src/page/calendar/component/month/component/MonthHeader.tsx
@@ -4,12 +4,20 @@ import calendar from "../../../../../constants/calendar.json";
 export const MonthHeader: React.FC = () => {
     const daysOfWeek = calendar.daysOfWeek;
     return (
-        <div className="grid grid-cols-7 gap-4 border px-4 py-4">
-            {daysOfWeek.map((day) => (
-                <div key={day} className="text-center font-semibold">
-                    {day}
-                </div>
-            ))}
+        <div className="grid grid-cols-7 bg-gray-100">
+            {daysOfWeek.map((day, idx) => {
+                let textColor = "";
+                if (idx === 0) {
+                    textColor = "text-red-500"; // 일요일
+                } else if (idx === 6) {
+                    textColor = "text-blue-500"; // 토요일
+                }
+                return (
+                    <div key={day} className={`text-center font-semibold ${textColor}`}>
+                        {day}
+                    </div>
+                );
+            })}
         </div>
     );
-}
+};
