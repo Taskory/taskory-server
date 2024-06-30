6be7da6e4f0cd997ad79ec482864f481bc8339e3
codeartist99 <ysw991106@gmail.com>
2024-06-30 16:20:20 +0900

    feat: fix temporary events

--- client/src/page/calendar/component/month/MonthCalendar.tsx
+++ client/src/page/calendar/component/month/MonthCalendar.tsx
@@ -52,36 +52,10 @@ export const MonthCalendar: React.FC = () => {
             tag: {id: 3, title: 'Review', color: 'YELLOW'},
             hashtags: [{id: 5, title: '#design'}, {id: 6, title: '#review'}],
             description: 'Review of the new design mockups',
-            startDateTime: '2024-07-17T13:00:00',
-            dueDateTime: '2024-07-17T14:00:00',
+            startDateTime: '2024-06-17T13:00:00',
+            dueDateTime: '2024-06-17T14:00:00',
             location: 'Conference Room C'
         },
-
-        ////////////////////////////
-        // This mock data makes an error
-        // : Cannot read properties of undefined (reading 'push')
-        // // event for single day (last day of a month)
-        // {
-        //     id: 4,
-        //     title: 'Client Meeting',
-        //     tag: { id: 1, title: 'Meeting', color: 'GREEN' },
-        //     hashtags: [{ id: 7, title: '#client' }, { id: 8, title: '#requirements' }],
-        //     description: 'Meeting with the client to discuss requirements',
-        //     startDateTime: '2024-07-31T15:00:00',
-        //     dueDateTime: '2024-07-31T16:30:00',
-        //     location: 'Client Office'
-        // },
-        // // event for single day (last day of Feburary)
-        // {
-        //     id: 5,
-        //     title: 'Team Drinks',
-        //     tag: { id: 4, title: 'Social', color: 'BLUE' },
-        //     hashtags: [{ id: 9, title: '#team' }, { id: 10, title: '#drinks' }],
-        //     description: 'Social event with the team',
-        //     startDateTime: '2024-02-29T17:00:00',
-        //     dueDateTime: '2024-02-29T19:00:00',
-        //     location: 'Local Bar'
-        // }
     ];
 
     const containerRef = useRef<HTMLDivElement>(null);
