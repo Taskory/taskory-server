b2c3a661531741a595f7b7b31e4710a44a9a9933
codeartist99 <ysw991106@gmail.com>
2024-07-06 20:05:55 +0900

    feat: view 2 columns in TimeCell component at WeekCalendar

--- client/src/page/calendar/component/component/TimeCell.tsx
+++ client/src/page/calendar/component/component/TimeCell.tsx
@@ -7,9 +7,9 @@ interface TimeCellProps {
 
 export const TimeCell: React.FC<TimeCellProps> = ({ events }) => {
     return (
-        <div className={`border h-20`}>
+        <div className={`w-full h-20 grid  grid-cols-${events.length} ${(events.length > 0) ? '' : 'border'}`}>
             {events.map((event, idx) => (
-                <div key={idx} className={`text-xs px-1 bg-${event.tag.color.toLowerCase()}-200`}>
+                <div key={idx} className={`col-span-1 p-1 text-xs bg-${event.tag.color.toLowerCase()}-200 w-full h-full`}>
                     {event.title}
                 </div>
             ))}
