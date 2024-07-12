83232ba25c5e4712a9b8078bc687c1130f915310
codeartist99 <ysw991106@gmail.com>
2024-07-12 15:01:47 +0900

    refactor: separate dummy data

--- client/src/page/calendar/component/WeekCalendar.tsx
+++ client/src/page/calendar/component/WeekCalendar.tsx
@@ -3,7 +3,7 @@ import { useCalendar } from "../context/CalendarContext";
 import { EventInterface } from "../../../api/interface/EventInterface";
 import { DayLine } from "./component/DayLine";
 import { WeekCalendarHeader } from "./component/WeekCalendarHeader";
-import {TimeCell} from "./component/TimeCell";
+import {useTestData} from "../context/TestDataContext";
 
 interface WeekInfoInterface {
     startSunday: Date;
@@ -11,6 +11,7 @@ interface WeekInfoInterface {
 
 export const WeekCalendar: React.FC = () => {
     const {currentDate} = useCalendar();
+    const {dummyEvents} = useTestData();
     const [events, setEvents] = useState<EventInterface[]>([]);
 
     const weekInfo: WeekInfoInterface = useMemo(() => ({
@@ -18,74 +19,8 @@ export const WeekCalendar: React.FC = () => {
     }), [currentDate]);
 
     useEffect(() => {
-        // Dummy data
-        const startOfWeek = new Date(currentDate.getFullYear(), currentDate.getMonth(), currentDate.getDate() - currentDate.getDay());
-        const endOfWeek = new Date(currentDate.getFullYear(), currentDate.getMonth(), currentDate.getDate() + (6 - currentDate.getDay()));
-        const dummyEvents: EventInterface[] = [
-            {
-                id: 1,
-                title: "Company Meeting",
-                tag: {id: 1, title: "Work", color: "yellow"},
-                hashtags: [{id: 1, title: "#meeting"}],
-                description: "Weekly team meeting to discuss contract updates.",
-                startDateTime: new Date(startOfWeek.getFullYear(), startOfWeek.getMonth(), startOfWeek.getDate() + 1, 10, 0).toISOString(),
-                dueDateTime: new Date(startOfWeek.getFullYear(), startOfWeek.getMonth(), startOfWeek.getDate() + 2, 11, 0).toISOString(),
-                location: "Conference Room B"
-            },
-            {
-                id: 2,
-                title: "Team Meeting",
-                tag: {id: 1, title: "Work", color: "blue"},
-                hashtags: [{id: 1, title: "#meeting"}],
-                description: "Weekly team meeting to discuss project updates.",
-                startDateTime: new Date(startOfWeek.getFullYear(), startOfWeek.getMonth(), startOfWeek.getDate() + 1, 10, 0).toISOString(),
-                dueDateTime: new Date(startOfWeek.getFullYear(), startOfWeek.getMonth(), startOfWeek.getDate() + 1, 11, 0).toISOString(),
-                location: "Conference Room A"
-            },
-            {
-                id: 3,
-                title: "Code Review",
-                tag: {id: 2, title: "Work", color: "green"},
-                hashtags: [{id: 2, title: "#code"}, {id: 3, title: "#review"}],
-                description: "Review the latest code commits.",
-                startDateTime: new Date(startOfWeek.getFullYear(), startOfWeek.getMonth(), startOfWeek.getDate() + 1, 14, 0).toISOString(),
-                dueDateTime: new Date(startOfWeek.getFullYear(), startOfWeek.getMonth(), startOfWeek.getDate() + 4, 15, 0).toISOString(),
-                location: "Zoom"
-            },
-            {
-                id: 4,
-                title: "Lunch with Client",
-                tag: {id: 3, title: "Personal", color: "red"},
-                hashtags: [{id: 4, title: "#lunch"}, {id: 5, title: "#client"}],
-                description: "Discuss project requirements with the client.",
-                startDateTime: new Date(startOfWeek.getFullYear(), startOfWeek.getMonth(), startOfWeek.getDate() + 3, 12, 30).toISOString(),
-                dueDateTime: new Date(startOfWeek.getFullYear(), startOfWeek.getMonth(), startOfWeek.getDate() + 3, 13, 30).toISOString(),
-                location: "Restaurant B"
-            },
-            {
-                id: 5,
-                title: "Yoga Class",
-                tag: {id: 4, title: "Health", color: "purple"},
-                hashtags: [{id: 6, title: "#yoga"}, {id: 7, title: "#fitness"}],
-                description: "Attend the weekly yoga class for relaxation.",
-                startDateTime: new Date(startOfWeek.getFullYear(), startOfWeek.getMonth(), startOfWeek.getDate() + 4, 18, 0).toISOString(),
-                dueDateTime: new Date(startOfWeek.getFullYear(), startOfWeek.getMonth(), startOfWeek.getDate() + 4, 19, 0).toISOString(),
-                location: "Gym"
-            },
-            {
-                id: 6,
-                title: "Project Deadline",
-                tag: {id: 5, title: "Work", color: "orange"},
-                hashtags: [{id: 8, title: "#deadline"}, {id: 9, title: "#project"}],
-                description: "Submit the final project deliverables.",
-                startDateTime: new Date(endOfWeek.getFullYear(), endOfWeek.getMonth(), endOfWeek.getDate(), 17, 0).toISOString(),
-                dueDateTime: new Date(endOfWeek.getFullYear(), endOfWeek.getMonth(), endOfWeek.getDate(), 23, 59).toISOString(),
-                location: "Office"
-            }
-        ];
-
         setEvents(dummyEvents);
-    }, [currentDate]);
+    }, [dummyEvents]);
 
     const getEventsForDay = useCallback((date: Date): {
         hourlyEvents: EventInterface[][],
@@ -164,13 +99,6 @@ export const WeekCalendar: React.FC = () => {
 
     function renderTimeLines() {
         return (
-            // <div className="grid grid-cols-1 h-full w-[10%] bg-gray-100">
-            //     {Array.from({length: 24}, (_, hour) => (
-            //         <div key={hour} className="flex-grow text-right text-xs h-20 pr-1">
-            //             {hour}:00
-            //         </div>
-            //     ))}
-            // </div>
             <div className="w-[10%]">
                 {Array.from({length: 24}, (_, hour) => (
                     <div key={hour} className="h-20 w-full bg-gray-100">
