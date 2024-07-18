43963f07349db6b9fcb83901be0deaac26cda833
codeartist99 <ysw991106@gmail.com>
2024-07-18 16:30:11 +0900

    feat: add temp request

--- client/src/api/EventApi.ts
+++ client/src/api/EventApi.ts
@@ -38,3 +38,26 @@ function getMonthFinalDate(date: Date): string {
     const day = String(tempDate.getDate()).padStart(2, '0'); // 일을 2자리로 맞춤
     return `${year}-${month}-${day}`;
 }
+
+export async function requestWeeklyEvents(date: Date) {
+    // const requestOptions = {
+    //     method: "GET",
+    //     headers: {
+    //         "Content-Type": "application/json",
+    //         "Authorization": `Bearer ${getAuthCookie()}`, // 비동기 함수 await 추가
+    //     },
+    // };
+    //
+    // try {
+    //     const startDateParam = `startDate=${getMonthFirstDate(date)}`;
+    //     const dueDateParam = `dueDate=${getMonthFinalDate(date)}`;
+    //     const response = await fetch(`${API_URL}/event?${startDateParam}&${dueDateParam}`, requestOptions);
+    //     if (response.ok) {
+    //         return await response.json();
+    //     } else {
+    //         console.error('Failed to fetch events:', response.statusText);
+    //     }
+    // } catch (error) {
+    //     console.error('Error during fetch:', error);
+    // }
+}
