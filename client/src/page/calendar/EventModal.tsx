b13e7f9cc627d791ea12bbedc361e7491bbe749c
codeartist99 <ysw991106@gmail.com>
2024-09-30 00:08:50 +0900

    refactor: modify design of event modal

--- client/src/page/calendar/EventModal.tsx
+++ client/src/page/calendar/EventModal.tsx
@@ -1,12 +1,12 @@
 import React, { useState, useEffect, KeyboardEvent } from 'react';
 import { API_URL } from "../../constants";
-import { format, addHours, isBefore } from 'date-fns'; // added isBefore for date comparison
+import { format, addHours, isBefore } from 'date-fns';
 import { getAllTags } from "../../api/tag/TagApi";
-import {createEvent, deleteEvent, getEventById, updateEvent} from "../../api/event/EventApi";
+import { createEvent, deleteEvent, getEventById, updateEvent } from "../../api/event/EventApi";
 import { SaveEventRequest, EventResponse } from "../../api/event/EventsTypes";
 import { TagResponse } from "../../api/tag/TagTypes";
 import { TimeUtil } from "../../util/TimeUtil";
-import {HashtagResponse} from "../../api/hashtag/HashtagTypes"; // Import the timezones from JSON
+import { HashtagResponse } from "../../api/hashtag/HashtagTypes";
 
 interface EventModalProps {
     isOpen: boolean;
@@ -14,7 +14,6 @@ interface EventModalProps {
     id?: number;
     refetchEvents: () => void;
 }
-
 
 const EventModal: React.FC<EventModalProps> = ({ isOpen, onClose, id, refetchEvents }) => {
     const [title, setTitle] = useState('');
@@ -28,7 +27,25 @@ const EventModal: React.FC<EventModalProps> = ({ isOpen, onClose, id, refetchEve
     const [dueDateTime, setDueDateTime] = useState('');
     const [location, setLocation] = useState('');
     const [loading, setLoading] = useState(false);
-    const [dateError, setDateError] = useState(''); // State for error message
+    const [dateError, setDateError] = useState('');
+
+    useEffect(() => {
+        if (isOpen) {
+            fetchTags();
+            if (id) {
+                fetchEvent(id);
+            } else {
+                const now = new Date();
+                const oneHourLater = addHours(now, 1);
+                const formattedStartDateTime = format(oneHourLater, 'yyyy-MM-dd\'T\'HH:00');
+                setStartDateTime(formattedStartDateTime);
+
+                const twoHoursLater = addHours(now, 2);
+                const formattedDueDateTime = format(twoHoursLater, 'yyyy-MM-dd\'T\'HH:00');
+                setDueDateTime(formattedDueDateTime);
+            }
+        }
+    }, [isOpen, id]);
 
     const fetchTags = async (): Promise<void> => {
         try {
@@ -67,31 +84,13 @@ const EventModal: React.FC<EventModalProps> = ({ isOpen, onClose, id, refetchEve
         }
     };
 
-    useEffect(() => {
-        if (isOpen) {
-            fetchTags();
-            if (id) {
-                fetchEvent(id);
-            } else {
-                const now = new Date();
-                const oneHourLater = addHours(now, 1);
-                const formattedStartDateTime = format(oneHourLater, 'yyyy-MM-dd\'T\'HH:00');
-                setStartDateTime(formattedStartDateTime);
-
-                const twoHoursLater = addHours(now, 2);
-                const formattedDueDateTime = format(twoHoursLater, 'yyyy-MM-dd\'T\'HH:00');
-                setDueDateTime(formattedDueDateTime);
-            }
-        }
-    }, [isOpen, id]);
-
     const handleStartDateTimeChange = (value: string): void => {
         setStartDateTime(value);
         const start = new Date(value);
         const due = addHours(start, 1);
         const formattedDueDateTime = format(due, 'yyyy-MM-dd\'T\'HH:00');
         setDueDateTime(formattedDueDateTime);
-        setDateError(''); // Clear error when start date changes
+        setDateError('');
     };
 
     const handleDueDateTimeChange = (value: string): void => {
@@ -101,7 +100,7 @@ const EventModal: React.FC<EventModalProps> = ({ isOpen, onClose, id, refetchEve
             setDateError('Due date cannot be before start date.');
         } else {
             setDueDateTime(value);
-            setDateError(''); // Clear error if the due date is valid
+            setDateError('');
         }
     };
 
@@ -186,8 +185,6 @@ const EventModal: React.FC<EventModalProps> = ({ isOpen, onClose, id, refetchEve
         onClose();
     };
 
-    if (!isOpen) return null;
-
     const handleDelete = async (): Promise<void> => {
         if (id) {
             try {
@@ -205,108 +202,128 @@ const EventModal: React.FC<EventModalProps> = ({ isOpen, onClose, id, refetchEve
             console.error('Missing required id');
         }
     };
+
+    if (!isOpen) return null;
+
     return (
         <dialog open={isOpen} className="modal">
-            <div className="modal-box">
+            <div className="modal-box max-w-md p-2">
                 {loading ? (
-                    <div>Loading...</div>
+                    <div className="flex justify-center items-center h-32">
+                        <span className="loading loading-spinner"></span>
+                    </div>
                 ) : (
                     <>
-                        <h3 className="font-bold text-lg">{id ? 'Edit Event' : 'Create New Event'}</h3>
-                        <div className="py-4 space-y-4">
-                            <div className="flex items-center">
-                                <label className="w-1/3">Event Title:</label>
+                        <div className="grid grid-cols-4">  {/* Updated: Create a 2-column grid layout */}
+                            <div className="col-span-3 py-2">
                                 <input
                                     type="text"
-                                    className="input input-bordered w-full"
-                                    value={title}
+                                    className="input input-ghost w-full font-bold"
+                                    value={id ? title : ''}
+                                    placeholder={"Type a title"}
                                     onChange={(e) => setTitle(e.target.value)}
                                 />
                             </div>
-
-                            <div className="flex items-center">
-                                <label className="w-1/3">Tag:</label>
-                                <select
-                                    className="select select-bordered w-full"
-                                    value={tagId !== undefined ? tagId : ''}
-                                    onChange={(e) => setTagId(Number(e.target.value))}
-                                >
-                                    <option value="">Select a Tag</option>
-                                    {tags.map(tag => (
-                                        <option key={tag.id} value={tag.id}>
-                                            {tag.title} ({tag.color})
-                                        </option>
-                                    ))}
-                                </select>
+                            <div className="col-span-1 content-center">
+                                <div className={"flex items-center"}>
+                                    {/* apply color*/}
+                                    <p className="text-red-600">●</p>
+                                    <select
+                                        className="select select-sm w-full ml-1"
+                                        value={tagId !== undefined ? tagId : ''}
+                                        onChange={(e) => setTagId(Number(e.target.value))}
+                                    >
+                                        <option value="">Tag</option>
+                                        {tags.map(tag => (
+                                            <option key={tag.id} value={tag.id}>
+                                                {tag.title} ({tag.color})
+                                            </option>
+                                        ))}
+                                    </select>
+                                </div>
                             </div>
-
-                            <div className="flex items-center">
-                                <label className="w-1/3">Hashtags:</label>
-                                <input
-                                    type="text"
-                                    placeholder="Type hashtag and press Enter"
-                                    className="input input-bordered w-full"
-                                    value={hashtagTitle}
-                                    onChange={(e) => setHashtagTitle(e.target.value)}
-                                    onKeyDown={handleHashtagKeyPress}
-                                />
+                            <div className="col-span-1">
+                                <label className="label text-sm justify-end mr-1">Date & Time</label>
                             </div>
-
-                            <div className="mt-2">
-                                {hashtags.map(hashtag => (
-                                    <span key={hashtag.id} className="badge badge-secondary m-1">
-                                        #{hashtag.name}
-                                    </span>
-                                ))}
-                            </div>
-
-                            <div className="flex items-center">
-                                <label className="w-1/3">Description:</label>
-                                <textarea
-                                    className="textarea textarea-bordered w-full"
-                                    value={description}
-                                    onChange={(e) => setDescription(e.target.value)}
-                                />
-                            </div>
-
-                            <div className="flex items-center">
-                                <label className="w-1/3">Start Date:</label>
+                            <div className="col-span-3 space-y-1 py-1">
                                 <input
                                     type="datetime-local"
-                                    className="input input-bordered w-full"
+                                    className="input input-bordered input-sm w-full"
                                     value={startDateTime}
                                     onChange={(e) => handleStartDateTimeChange(e.target.value)}
                                 />
-                            </div>
-
-                            <div className="flex items-center">
-                                <label className="w-1/3">Due Date:</label>
                                 <input
                                     type="datetime-local"
-                                    className="input input-bordered w-full"
+                                    className="input input-bordered input-sm w-full"
                                     value={dueDateTime}
                                     onChange={(e) => handleDueDateTimeChange(e.target.value)}
                                 />
                             </div>
+                            <div className="col-span-1">
+                                <label className="label text-sm justify-end mr-1">Hashtags</label>
+                            </div>
+                            <div className="col-span-3">
+                                <div>
+                                    <input
+                                        type="text"
+                                        placeholder="Type hashtag and press Enter"
+                                        className="input input-bordered input-sm w-full"
+                                        value={hashtagTitle}
+                                        onChange={(e) => setHashtagTitle(e.target.value)}
+                                        onKeyDown={handleHashtagKeyPress}
+                                    />
+                                    <div className="mt-1 flex flex-wrap">
+                                        {hashtags.map(hashtag => (
+                                            <span key={hashtag.id} className="badge badge-secondary m-1">
+                                    #{hashtag.name}
+                                </span>
+                                        ))}
+                                    </div>
+                                </div>
+                            </div>
+                            <div className="col-span-4">
 
-                            {/* Display error message if dates are invalid */}
-                            {dateError && <div className="text-red-500 text-sm">{dateError}</div>}
+                                {dateError && (
+                                    <div className="col-span-3 text-red-500 text-xs">
+                                        {dateError}
+                                    </div>
+                                )}
+                            </div>
+                            <div className="col-span-1">
 
-                            <div className="flex items-center">
-                                <label className="w-1/3">Location:</label>
+                                <label className="label text-sm justify-end mr-1">Location</label>
+                            </div>
+                            <div className="col-span-3">
+
                                 <input
                                     type="text"
-                                    className="input input-bordered w-full"
+                                    className="input input-bordered input-sm w-full"
                                     value={location}
                                     onChange={(e) => setLocation(e.target.value)}
                                 />
                             </div>
-                        </div>
+                            <div className="col-span-1">
 
-                        <div className="modal-action">
-                            <button className="btn" onClick={handleSave}>{id ? 'Update' : 'Save'}</button>
-                            <button className="btn btn-error" onClick={handleClose}>Cancel</button>
-                            <button className="btn btn-error" onClick={handleDelete}>Delete</button>
+                                <label className="label text-sm justify-end mr-1">Description</label>
+                            </div>
+                            <div className="col-span-3">
+
+                                <textarea
+                                    className="textarea textarea-bordered textarea-sm w-full"
+                                    value={description}
+                                    onChange={(e) => setDescription(e.target.value)}
+                                    rows={2}
+                                />
+                            </div>
+                        </div>
+                        <div className="flex gap-2 justify-end">
+                            <button className="btn btn-primary btn-sm" onClick={handleSave}>
+                                {id ? 'Update' : 'Save'}
+                            </button>
+                            {id && (
+                                <button className="btn btn-error btn-sm" onClick={handleDelete}>Delete</button>
+                            )}
+                            <button className="btn btn-outline btn-sm" onClick={handleClose}>Cancel</button>
                         </div>
                     </>
                 )}
