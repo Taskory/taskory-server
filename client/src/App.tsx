47a1a2ee0dee45ba0c0327f54a2737596550f454
codeartist99 <ysw991106@gmail.com>
2024-10-12 18:59:56 +0900

    refactor: remove unneeded feature

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,26 +1,10 @@
-import React, { useEffect } from 'react';
+import React from 'react';
 import {AppRouter} from "./router/AppRouter";
 import {SidebarStateProvider} from "./context/SidebarStateContext";
 import {CalendarViewProvider} from "./page/calendar/context/CalendarViewContext";
 import {EventContextProvider} from "./context/EventContext";
 
 const App = () => {
-    useEffect(() => {
-        // Drag prevention event handler
-        const preventDefault = (e: Event) => {
-            e.preventDefault();
-        };
-
-        // Register drag prevention event listeners
-        window.addEventListener('dragstart', preventDefault);
-        window.addEventListener('drop', preventDefault);
-
-        // Cleanup function
-        return () => {
-            window.removeEventListener('dragstart', preventDefault);
-            window.removeEventListener('drop', preventDefault);
-        };
-    }, []);
 
     return (
         <SidebarStateProvider>
