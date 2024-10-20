53ff8aefc772c3ddfdcdbdcbfe098397d9974daf
codeartist99 <ysw991106@gmail.com>
2024-10-21 01:46:21 +0900

    refactor: remove unused context

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -2,17 +2,14 @@ import React from 'react';
 import {AppRouter} from "./router/AppRouter";
 import {SidebarStateProvider} from "./context/SidebarStateContext";
 import {CalendarViewProvider} from "./page/calendar/context/CalendarViewContext";
-import {EventContextProvider} from "./context/EventContext";
 
 const App = () => {
 
     return (
         <SidebarStateProvider>
-            <EventContextProvider>
-                <CalendarViewProvider>
-                    <AppRouter/>
-                </CalendarViewProvider>
-            </EventContextProvider>
+            <CalendarViewProvider>
+                <AppRouter/>
+            </CalendarViewProvider>
         </SidebarStateProvider>
     );
 }
