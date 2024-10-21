561a1ced5728c730e61ddd882b7a532045adf3a0
codeartist99 <ysw991106@gmail.com>
2024-10-21 18:27:36 +0900

    fix: fixed not working update a color state

--- client/src/component/ColorSelectBox.tsx
+++ client/src/component/ColorSelectBox.tsx
@@ -1,5 +1,5 @@
 import {getTagBGColor} from "../util/TagUtil";
-import {useState} from "react";
+import React, {useState} from "react";
 import {TagColor} from "../api/tag/TagTypes";
 
 interface ColorSelectBoxProps {
