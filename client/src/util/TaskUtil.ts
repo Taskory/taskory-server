71c1cb021d1db9fae9567073853f9af5dcd39903
codeartist99 <ysw991106@gmail.com>
2024-12-09 14:38:01 +0900

    refactor: update calculate progress rate to display integer value

--- client/src/util/TaskUtil.ts
+++ client/src/util/TaskUtil.ts
@@ -2,7 +2,7 @@ import {TaskStatus, TaskSummary} from "../api/task/TaskTypes";
 
 export function calculateProgressRate(task: TaskSummary): number {
     if (task.itemsCount > 0) {
-        return task.completedItemsCount / task.itemsCount * 100;
+        return Math.round((task.completedItemsCount / task.itemsCount) * 100);
     } else {
         if (task.status === TaskStatus.DONE) {
             return 100;
